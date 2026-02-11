60fd3622433302cc4c0987298922d93ffac7f623
salwazahramunir <salwazahramunir@gmail.com>
2026-02-11 11:24:08 +0700

    add service type inside options enum

--- src/option/dto/option.dto.ts
+++ src/option/dto/option.dto.ts
@@ -7,6 +7,7 @@ export enum CategoryOption {
   MEMBER = 'member category', // untuk kategori member hewan (contoh: "Regular", "VIP - Store", "VIP - Home", "Influencer")
   CUSTOMER = 'customer category', // untuk kategori customer (contoh: "Prioritas")
   PET_TYPE = 'pet type', // untuk jenis hewan (contoh: "Dog", "Cat", "Other")
+  SERVICE_TYPE = 'service type', // untuk jenis layanan/product (contoh: "Grooming", "Hotel", "Addon")
 }
 
 export class GetOptionQueryDto {
