0aec469bef7e9a6c2162df4539400da1ddd73616
salwazahramunir <salwazahramunir@gmail.com>
2026-04-13 19:09:00 +0700

    feat(option): add MEMBER category to CategoryOption enum

--- src/option/dto/option.dto.ts
+++ src/option/dto/option.dto.ts
@@ -4,9 +4,9 @@ export enum CategoryOption {
   HAIR = 'hair category', // untuk jenis bulu hewan (contoh: "Short Hair", "Long Hair")
   SIZE = 'size category', // untuk ukuran hewan (contoh: "Small", "Medium", "Large")
   BREED = 'breed category', // untuk keturunan hewan (contoh: "Pom", "Ras Mix")
-  MEMBER = 'member category', // untuk kategori member hewan (contoh: "Regular", "VIP - Store", "VIP - Home", "Influencer")
   CUSTOMER = 'customer category', // untuk kategori customer (contoh: "Prioritas")
   PET_TYPE = 'pet type', // untuk jenis hewan (contoh: "Dog", "Cat", "Other")
+  SESSION_SKILL = 'session - skill', // untuk skill groomer dalam session (contoh: "Basic Grooming", "Advanced Styling")
 }
 
 export class GetOptionQueryDto {
